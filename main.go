234f92dbd547938fb307861b70ef7f32d686dc0e
ahmedsport <ahmedsport@hotmail.com>
2023-01-31 00:01:56 +0000

    hello world

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var whatToSay string = "Hello everyone!"
+	sayHelloWorl(whatToSay)
+}
+
+func sayHelloWorl(whatToSay string ){
+	fmt.Println(whatToSay)
+
+}
+
